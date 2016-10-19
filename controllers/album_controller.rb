ce5b1e5e209c1c99a092b71546706e0ec7f5c485
Josephine Elder <parusmontanus@gmail.com>
2016-10-19 15:22:25 +0100

    Completed artist routes

--- controllers/album_controller.rb
+++ controllers/album_controller.rb
@@ -2,9 +2,20 @@ require_relative '../models/album.rb'
 
 get '/albums' do
   @albums = Album.all()
-  erb(:index)
+  erb(:'album/index')
 end
 
 get '/albums/new' do
-  erb(:new)
+  erb(:'album/new')
+end
+
+post '/albums' do
+  @album = Album.new(params)
+  @album.save()
+  erb(:'album/create')
+end
+
+get '/albums/:id' do
+  @album = Album.find(params[:id])
+  erb(:'album/show')
 end
